e1a604e2ad03360c559c135f67f5bc1a4831df30
Corbin Lazarone <corbinlazarone1@gmail.com>
2025-12-11 20:45:54 -0600

    wip: event validator

--- server/cmd/internals/validator/event.go
+++ server/cmd/internals/validator/event.go
@@ -1,11 +1,17 @@
 package validator
 
+import (
+	"errors"
+	"time"
+)
+
 // Validator user's and AI's Google Calendar events.
 
 type Event struct {
 	AssignmentID   int    `json:"assignment_id"`
 	AssignmentName string `json:"assignment_name"`
 	DueDate        string `json:"due_date"`
+	AllDay         bool   `json:"all_day"`
 	StartTime      string `json:"start_time"`
 	EndTime        string `json:"end_time"`
 	Reminder       int    `json:"reminder"`
@@ -13,43 +19,93 @@ type Event struct {
 }
 
 type EventError struct {
-	AssignmentID   int    `json:"assignment_id"`
-	AssignmentName string `json:"assignment_name"`
-	Error          string `json:"error"`
+	AssignmentID   int      `json:"assignment_id"`
+	AssignmentName string   `json:"assignment_name"`
+	Errors         []string `json:"errors"`
 }
 
-// TODO: validate that due date is valid date time string to match google calendar API requirements
-// TODO: validate that start time is valid time string to match google calendar API requirements
-// TODO: validate that end time is valid time string to match google calendar API requirements
-// TODO: validate that reminder is valid integer to match google calendar API requirements
-// TODO: validate that color is valid hex color to match google calendar API requirements
+func (e *Event) validateDueDate() error {
+	res := checkFormat(e.DueDate, time.DateOnly)
+
+	if !res && e.AllDay {
+		return errors.New("Due date does not match expected 'YYYY-MM-DD' format")
+	}
+
+	if res && !e.AllDay {
+		return errors.New("All day event is not marked")
+	}
+
+	return nil
+}
+
+func (e *Event) validateDateTimes() error {
+	resStart := checkFormat(e.StartTime, time.RFC3339)
+	if !resStart {
+		return errors.New("Start time does not match expected 'YYYY-MM-DDTHH:MM:SSZ' format")
+	}
+
+	resEnd := checkFormat(e.EndTime, time.RFC3339)
+	if !resEnd {
+		return errors.New("Start time does not match expected 'YYYY-MM-DDTHH:MM:SSZ' format")
+	}
+
+	return nil
+}
+
+// func (e *Event) validateReminder() error {
+// 	return nil
+// }
+
+// func (e *Event) validateColor() error {
+// 	return nil
+// }
 
 func (e *Event) Validate() []EventError {
-	return []EventError{
-		{
-			AssignmentID:   1,
-			AssignmentName: "Assignment 1",
-			Error:          "Due date is invalid",
-		},
-		{
-			AssignmentID:   2,
-			AssignmentName: "Assignment 2",
-			Error:          "Start time is invalid",
-		},
-		{
-			AssignmentID:   3,
-			AssignmentName: "Assignment 3",
-			Error:          "End time is invalid",
-		},
-		{
-			AssignmentID:   4,
-			AssignmentName: "Assignment 4",
-			Error:          "Reminder is invalid",
-		},
-		{
-			AssignmentID:   5,
-			AssignmentName: "Assignment 5",
-			Error:          "Color is invalid",
-		},
+	var errs []EventError
+
+	err := e.validateDueDate()
+	if err != nil {
+		errs = append(errs, EventError{
+			AssignmentID:   e.AssignmentID,
+			AssignmentName: e.AssignmentName,
+			Errors:         []string{err.Error()},
+		})
 	}
+
+	err = e.validateDateTimes()
+	if err != nil {
+		errs = append(errs, EventError{
+			AssignmentID:   e.AssignmentID,
+			AssignmentName: e.AssignmentName,
+			Errors:         []string{err.Error()},
+		})
+	}
+
+	// err = e.validateReminder()
+	// if err != nil {
+	// 	errs = append(errs, EventError{
+	// 		AssignmentID:   e.AssignmentID,
+	// 		AssignmentName: e.AssignmentName,
+	// 		Errors:         []string{err.Error()},
+	// 	})
+	// }
+
+	// err = e.validateColor()
+	// if err != nil {
+	// 	errs = append(errs, EventError{
+	// 		AssignmentID:   e.AssignmentID,
+	// 		AssignmentName: e.AssignmentName,
+	// 		Errors:         []string{err.Error()},
+	// 	})
+	// }
+
+	return errs
+}
+
+func checkFormat(current, expected string) bool {
+	_, err := time.Parse(expected, current)
+	if err != nil {
+		return false
+	}
+	return true
 }
