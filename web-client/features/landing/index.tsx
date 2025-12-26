1ce6ca742508a2842884cd72fc7490882b8fa70f
Corbin Lazarone <corbinlazarone1@gmail.com>
2025-12-25 23:11:06 -0600

    refractor: landing page navigation button for easier shortcuts when in dev

--- web-client/features/landing/index.tsx
+++ web-client/features/landing/index.tsx
@@ -1,7 +1,18 @@
-export function Landing() {
+"use client";
+
+import { Button } from "@/shared/ui/button";
+import { useRouter } from "next/navigation";
+
+export function Landing({ isLoggedIn }: { isLoggedIn: boolean }) {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Todue Landing Page</h1>
+      {isLoggedIn ? (
+        <Button onClick={() => router.push("/dashboard")}>Dashboard</Button>
+      ) : (
+        <Button onClick={() => router.push("/login")}>Sign in</Button>
+      )}
     </div>
   );
 }
