01bd4cc196e8e6abefdce637190454d8569b48c8
Corbin Lazarone <corbinlazarone1@gmail.com>
2026-03-09 08:18:31 -0500

    removed tos and privacy policy and changed readme

--- web-client/features/auth/components/login-form.tsx
+++ web-client/features/auth/components/login-form.tsx
@@ -89,10 +89,6 @@ export function LoginForm({
           </form>
         </CardContent>
       </Card>
-      <FieldDescription className="px-6 text-center">
-        By clicking creating an account, you agree to our{" "}
-        <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-      </FieldDescription>
     </div>
   );
 }
